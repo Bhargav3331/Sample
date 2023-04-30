c9fa5a396c8e7aa3156115376dee33b33acd543d
Bhargav3331 <bhargavreddy.patil@gmail.com>
2023-04-30 08:42:30 -0400

    Initial commit

---
+++ SamplePackage/Main.java
@@ -0,0 +1,11 @@
+package SamplePackage;
+
+public class Main {
+    public static void main(String[] args) {
+        PersonName myObj = new PersonName();
+        myObj.setName("John");
+        System.out.println(myObj.getName());
+        myObj.setName("Bhargav");
+        System.out.println(myObj.getName());
+    }
+}
---
+++ SamplePackage/PersonName.java
@@ -0,0 +1,13 @@
+package SamplePackage;
+public class PersonName {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+}
